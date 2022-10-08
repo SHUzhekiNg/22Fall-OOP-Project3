ae9ee583d9810caf1c679c9fe57e784a39bbbdb9
郑力铖 <92080988+SHUzhekiNg@users.noreply.github.com>
2022-10-08 10:19:05 +0800

    打打样子

--- 22Fall-OOP-Project3/myvector.cpp
+++ 22Fall-OOP-Project3/myvector.cpp
@@ -1,1 +1,50 @@
 #include "myvector.h"
+template<typename T>
+MyVector<T>::MyVector()
+{
+}
+
+template<typename T>
+MyVector<T>::~MyVector()
+{
+}
+
+template<typename T>
+MyVector<T>::MyVector(const MyVector& v)
+{
+}
+
+template<typename T>
+MyVector<T>::MyVector(const T* v)
+{
+}
+
+template<typename T>
+void MyVector<T>::push_back(T dt)
+{
+}
+
+template<typename T>
+bool MyVector<T>::operator==(const MyVector<T>& a)
+{
+	return false; // TODO:
+}
+
+template<typename T>
+T& MyVector<T>::operator[](const int& index) const
+{
+	if (index > l) cerr << "下标越界！" << endl;
+	else return data[index];
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const MyVector<T>& v)
+{
+	// TODO: 在此处插入 return 语句
+}
+
+template<typename T>
+istream& operator>>(istream& in, MyVector<T>& v)
+{
+	// TODO: 在此处插入 return 语句
+}
--- 22Fall-OOP-Project3/myvector.h
+++ 22Fall-OOP-Project3/myvector.h
@@ -1,18 +1,29 @@
 #pragma once
+#ifndef MYVECTOR_H_
+#define MYVECTOR_H_
+
 #include <iostream>
 using namespace std;
 
 template <typename T>
 class MyVector{
 public:
-	MyVector();
+	MyVector():data(nullptr),l(0);
 	~MyVector();
 	MyVector(const MyVector& v);
+	MyVector(const T* v);
 
-	bool operator== (const MyVector<T>& a, const MyVector<T>& b);
-	friend T& operator [](const int& index);
+	void push_back(T dt);
+	int size(void) const { return l; }
 
-
+	friend ostream& operator<<(ostream& out, const MyVector<T>& v);
+	friend istream& operator>>(istream& in, MyVector<T>& v);
+	bool operator== (const MyVector<T>& a);
+	T& operator[] (const int& index) const;
+	
 private:
 	T* data;
+	int l;
 };
+
+#endif // !MYVECTOR_H_
