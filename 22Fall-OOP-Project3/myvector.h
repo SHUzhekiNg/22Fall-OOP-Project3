5cc62661ffa710d0cd964315cc9346a061008b8c
LeeXzhou <109325527+LeeXzhou@users.noreply.github.com>
2022-10-15 17:22:15 +0800

    version1.0

--- 22Fall-OOP-Project3/myvector.h
+++ 22Fall-OOP-Project3/myvector.h
@@ -16,8 +16,19 @@ public:
 	int size(void) const { return length; }
 	void resize(const int& s);
 
-	friend ostream& operator<<(ostream& out, const MyVector& v);
-	friend istream& operator>>(istream& in, MyVector& v);
+	friend ostream& operator<<(ostream& out, const MyVector& v) {
+		if (v.length == 0)
+			out << "()";
+		else
+		{
+			out << "(" << v.data[0];
+			for (int i = 1; i < v.length; i++)
+				out << ", " << v.data[i];
+			out << ")";
+		}
+		return out;
+	}
+	//friend istream& operator>>(istream& in, MyVector& v);
 	MyVector& operator=(const MyVector& v);
 	bool operator== (const MyVector& a);
 	T& operator[] (const int& index) const;
@@ -26,5 +37,100 @@ private:
 	T* data;
 	int length;
 };
+template<typename T>
+MyVector<T>::~MyVector()
+{
+	length = 0;
+	if (data != nullptr) delete[] data;
+}
+
+template<typename T>
+MyVector<T>::MyVector(const MyVector& v)
+{
+	length = 0;
+	data = nullptr;
+	*this = v;
+}
+
+template<typename T>
+MyVector<T>::MyVector(int ll, const T* v)
+{
+	length = (ll > 0) ? ll : 0;
+	data = nullptr;
+	if (length > 0)
+	{
+		data = new T[length];
+		for (int i = 0; i < length; i++)
+			data[i] = (v == nullptr) ? 0 : v[i];
+	}
+}
+
+template<typename T>
+void MyVector<T>::push_back(T dt)
+{
+	T *dx = new T[length + 1];
+	for (int i = 0; i < length; i++) {
+		dx[i] = data[i];
+	}
+	delete[] data;
+	dx[length] = dt;
+	data = dx;
+	length++;
+}
+
+template<typename T>
+void MyVector<T>::resize(const int& s)
+{
+	if (s < 0 || s == length)
+		return;
+	else if (s == 0)
+	{
+		if (data != nullptr) delete[] data;
+		data = nullptr;
+		length = 0;
+	}
+	else
+	{
+		T* temp = data;
+		data = new T[s];
+		for (int i = 0; i < s; i++)
+			data[i] = (i < length) ? temp[i] : 0;
+		length = s;
+		delete[] temp;
+	}
+}
+
+template<typename T>
+MyVector<T>& MyVector<T>::operator=(const MyVector<T>& v)
+{
+	if (length != v.length)
+	{
+		if (data != nullptr) delete[] data;
+		length = v.length;
+		data = new T[length];
+	}
+	for (int i = 0; i < length; i++)
+		data[i] = v.data[i];
+	return *this;
+}
+
+template<typename T>
+bool MyVector<T>::operator==(const MyVector<T>& a)
+{
+	if (a.length != length) return false;
+	else
+		for (int i = 0; i < length; i++)
+			if (a[i] != data[i]) return false;
+	return true;
+}
+
+template<typename T>
+T& MyVector<T>::operator[](const int& index) const
+{
+	if (index > length || index < 0) cerr << "下标越界！" << endl;
+	if (data == nullptr) cerr << "向量为空指针！" << endl;
+	else return data[index];
+}
+
 
 #endif // !MYVECTOR_H_
--- 22Fall-OOP-Project3/test.cpp
+++ 22Fall-OOP-Project3/test.cpp
@@ -1,11 +1,71 @@
-#include "myvector.cpp"
+#include "myvector.h"
 int main() 
 {
-	int a[10];
+	int a[10], b[10], num = 0, c = 1, length;	//num是向vec中添加的数据，length是修改size
 	for (int i = 0; i < 10; i++)
 	{
 		a[i] = i;
+		b[i] = 2 * i;
 	}
-	MyVector<int> vec1(10,a);
+	MyVector<int> vec1(10, a);
+	MyVector<int> vec2(10, b);
+	while (c) {
+		cout << "当前vector为:vec1=" << vec1 << ", vec2=" << vec2 << endl << "请选择测试操作:" << endl;
+		cout << endl << "1. 测试关系运算符==";
+		cout << endl << "2. 获取vec1和vec2的容量";
+		cout << endl << "3. 向vec1中添加数据";
+		cout << endl << "4. 向vec2中添加数据";
+		cout << endl << "5. 测试赋值运算符，将vec2赋值给vec1";
+		cout << endl << "6. 修改vec1容量大小";
+		cout << endl << "7. 修改vec2容量大小";
+		cout << endl << "0. 退出";
+		cout << endl << "选择功能 (0~6):";
+		cin >> c;
+		switch (c) {
+		case 1:
+			cout << "vec1为：";
+			cout << vec1 << endl;
+			cout << "vec2为：";
+			cout << vec2 << endl;
+			if (vec1 == vec2)
+				cout << "vec1和vec2相等。" << endl;
+			else
+				cout << "vec1和vec2不相等。" << endl;
+			break;
+		case 2:
+			cout << "vec1容量为：";
+			cout << vec1.size() << endl;
+			cout << "vec2容量为：";
+			cout << vec2.size() << endl;
+			break;
+		case 3:
+			cout << "请输入您想要添加的数据";
+			cin >> num;
+			vec1.push_back(num);
+			break;
+		case 4:
+			cout << "请输入您想要添加的数据";
+			cin >> num;
+			vec2.push_back(num);
+			break;
+		case 5:
+			vec1 = vec2;
+			break;
+		case 6:
+			cout << "请输入vec1修改后的大小:";
+			cin >> length;
+			vec1.resize(length);
+			break;
+		case 7:
+			cout << "请输入vec2修改后的大小:";
+			cin >> length;
+			vec2.resize(length);
+			break;
+		default:
+			break;
+		}
+	}
+	system("pause");
+	
 	return 0;
 }
